2f68a616a380c10646e12f9096f0b2dc231a26da
Hardeep Singh <32304845+Inventiveatsquare@users.noreply.github.com>
2020-09-27 01:03:07 +0530

    Update ContentArea.js

--- src/UI_Components/ContentArea.js
+++ src/UI_Components/ContentArea.js
@@ -8,15 +8,24 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 export default class ContentArea extends PureComponent {
+
+
+
+
   constructor() {
     super()
 
     this.state = {
+      quantity: 1,
+      show: true,
+      max: 5,
+      min: 0,
       items: [...Array(10).keys()],
       hasOverflow: false,
       canScrollLeft: false,
       canScrollRight: false
     }
+    
 
     this.handleClickAddItem = this.handleClickAddItem.bind(this)
     this.handleClickRemoveItem = this.handleClickRemoveItem.bind(this)
@@ -32,6 +41,45 @@ export default class ContentArea extends PureComponent {
 
     this.container = null
   }
+
+
+  IncrementItem = () => {
+    this.setState(prevState => {
+      if(prevState.quantity < 9) {
+        return {
+          quantity: prevState.quantity + 1
+        }
+      } else {
+        return null;
+      }
+    });
+}
+
+DecreaseItem = () => {
+  this.setState(prevState => {
+    if(prevState.quantity > 0) {
+      return {
+        quantity: prevState.quantity - 1
+      }
+    } else {
+      return null;
+    }
+  });
+}
+
+ToggleClick = () => {
+  this.setState({
+    show: !this.state.show
+  });
+}
+
+handleChange = (event) => {
+  this.setState({quantity: event.target.value});
+}
+
+
+
+
 
   componentDidMount() {
     this.checkForOverflow()
